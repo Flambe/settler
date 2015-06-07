d809964b6b44c7f3a737466a8671aa81d9c529bc
Steve Axtmann <steve.axtmann+github@gmail.com>
2015-06-07 15:06:08 +0200

    added mongo

--- scripts/provision.sh
+++ scripts/provision.sh
@@ -213,6 +213,44 @@ sudo -u postgres psql -c "CREATE ROLE homestead LOGIN UNENCRYPTED PASSWORD 'secr
 sudo -u postgres /usr/bin/createdb --echo --owner=homestead homestead
 service postgresql restart
 
+# Install MongoDB
+
+#!/usr/bin/env bash
+
+echo ">>> Installing MongoDB"
+
+# Get key and add to sources
+sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 7F0CEB10
+echo 'deb http://downloads-distro.mongodb.org/repo/ubuntu-upstart dist 10gen' | sudo tee /etc/apt/sources.list.d/mongodb.list
+
+# Update
+sudo apt-get update
+
+# Install MongoDB
+# -qq implies -y --force-yes
+sudo apt-get install -qq mongodb-10gen
+
+# Test if PHP is installed
+php -v > /dev/null 2>&1
+PHP_IS_INSTALLED=$?
+
+if [ $PHP_IS_INSTALLED -eq 0 ]; then
+    # install dependencies
+    sudo apt-get -y install php-pear php5-dev
+
+    # install php extencion
+    echo "no" > answers.txt
+    sudo pecl install mongo < answers.txt
+    rm answers.txt
+
+    # add extencion file and restart service
+    echo 'extension=mongo.so' | sudo tee /etc/php5/mods-available/mongo.ini
+
+    sudo ln -s /etc/php5/mods-available/mongo.ini /etc/php5/fpm/conf.d/mongo.ini
+    sudo ln -s /etc/php5/mods-available/mongo.ini /etc/php5/cli/conf.d/mongo.ini
+    sudo service php5-fpm restart
+fi
+
 # Install Blackfire
 
 apt-get install -y blackfire-agent blackfire-php
